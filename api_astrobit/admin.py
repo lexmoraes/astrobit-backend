4216b241cf3266172231d646d2c5ff48b82055b8
lexmoraes <lexfisica@gmail.com>
2024-12-01 22:30:16 -0400

    release v1.2.0

--- api_astrobit/admin.py
+++ api_astrobit/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, RankUser, GameCardData
+
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
@@ -17,6 +18,22 @@ class CustomUserAdmin(UserAdmin):
             'fields': ('email', 'username', 'password1', 'password2')}
         ),
     )
+
+
+# Registro do modelo RankUser
+@admin.register(RankUser)
+class RankUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'placement', 'username', 'score', 'created_at')
+    search_fields = ('username__username',)
+    list_filter = ('placement', 'score')
+
+# Registro do modelo GameCardData
+@admin.register(GameCardData)
+class GameCardDataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'game_title', 'author', 'description', 'link', 'created_at')
+    search_fields = ('game_title', 'author__username', 'description')
+    list_filter = ('created_at',)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
