462008a3d1163f8ee8c248e250c9a908f5e6d20b
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2024-12-16 18:50:56 -0400

    Update settings.py

--- main_configurations/settings.py
+++ main_configurations/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configurações opcionais para arquivos de mídia (se futuramente quiser aceitar uploads também)
 MEDIA_URL = '/media/'
